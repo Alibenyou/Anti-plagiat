ccc231df27b6ba37973da0b09447b934031ba02c
1COURONNE <rdivin1950@gmail.com>
2026-01-29 14:10:37 +0100

    Résolution des conflits Header + navigation

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import Home from "./screens/home";
 import Pertinence from "./screens/Pertinence";
 import Student from "./screens/Student";
 import Teacher from "./screens/Teacher";
 import Admin from "./screens/Admin";
+import Inscription from "./screens/inscription";
+import Connexion from "./screens/connexion";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/inscription" element={<Inscription />} />
+        <Route path="/connexion" element={<Connexion />} />
         <Route path="/student" element={<Student />} />
         <Route path="/teacher" element={<Teacher />} />
         <Route path="/pertinence" element={<Pertinence />} />
