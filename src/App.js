86fa7e1c3eddf2775f5a6bbb436a81c2b539a692
rooseveltlacouronne <roosevelt.tbr@gmail.com>
2026-01-29 16:54:19 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
-import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Inscription from './screens/inscription';
-import Connexion from './screens/connexion';
-import './App.css';
-import Home from './screens/home' 
-import Student from './screens/Student';
-import Teacher from './screens/Teacher';
-import Admin from './screens/Admin';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./screens/home";
+import Pertinence from "./screens/Pertinence";
+import Student from "./screens/Student";
+import Teacher from "./screens/Teacher";
+import Admin from "./screens/Admin";
+import Inscription from "./screens/inscription";
+import Connexion from "./screens/connexion";
 
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
+    <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/student" element={<Student/>}/>
-        <Route path="/teacher" element={<Teacher/>}/>
-        <Route path="/admin" element={<Admin/>}/>
+        <Route path="/" element={<Home />} />
         <Route path="/inscription" element={<Inscription />} />
         <Route path="/connexion" element={<Connexion />} />
+        <Route path="/student" element={<Student />} />
+        <Route path="/teacher" element={<Teacher />} />
+        <Route path="/pertinence" element={<Pertinence />} />
+        <Route path="/admin" element={<Admin />} />
       </Routes>
-      </BrowserRouter>
-    </div>
+    </BrowserRouter>
   );
 }
 
